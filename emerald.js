495033d97e3402db270418fea8a148cad0fc96e6
Lars Ingebrigtsen <larsi@gnus.org>
2017-08-09 12:09:35 +0200

    Don't list relistings under "first issues".

--- emerald.js
+++ emerald.js
@@ -315,9 +315,13 @@ function wanted(comic) {
   // This is rather confused logic, but I wanted the "list first
   // issues and graphic novels" logic, which is what I want...
   if ($.inArray("ones", activeCategories) != -1) {
-    if (comic.issue == "#1" || comic.issue == "VOL 01")
-      return true;
-    else {
+    if (comic.issue == "#1" || comic.issue == "VOL 01") {
+      if ($.inArray("relistings", activeCategories) == -1 &&
+	  (comic.original || comic["class"] == "SS" || comic["class"] == "OA"))
+	return false;
+      else
+	return true;
+    } else {
       if ($.inArray("resolicitations", activeCategories) == -1 &&
 	  comic.resolicited)
 	return false;
